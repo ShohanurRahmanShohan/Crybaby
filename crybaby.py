c68c65af1d42e6b7750f79b2fd71cf19f72fad71
Shohanur Rahman <78851871+ShohanurRahmanShohan@users.noreply.github.com>
2024-10-09 17:22:46 +0600

    Update crybaby.py

--- crybaby.py
+++ crybaby.py
@@ -1,28 +1,11 @@
-# Replace the data_table with the following code to generate a unique mapping:
-# 
-# import random
-# 
-# def generate_mapping_table():
-#     """Generate a character-to-number mapping table."""
-#     mapping_table = []
-#     unique_numbers = set()
-# 
-#     # Define the range of characters you want to include
-#     for char in range(32, 127):  # ASCII characters from space (32) to ~ (126)
-#         while True:
-#             # Generate a unique 4-digit number
-#             number = random.randint(1000, 9999)
-#             if number not in unique_numbers:
-#                 unique_numbers.add(number)
-#                 mapping_table.append((chr(char), number))
-#                 break
-# 
-#     return mapping_table
-# 
-# # Generate the mapping table and replace the data_table variable
-# data_table = generate_mapping_table()
 
-# Example function to demonstrate encryption and decryption (remains unchanged)
+
+
+# Generate the mapping table and replace the data_table variable, look at readme for the code to generate the data 
+
+
+char_to_number_dict = dict(data_table)
+number_to_char_dict = {str(value): key for key, value in data_table}
 def get_concatenated_number_from_string(input_string):
     concatenated_number_str = ''.join(str(char_to_number_dict.get(char, '')) for char in input_string)
     return int(concatenated_number_str) if concatenated_number_str else None
@@ -55,5 +38,3 @@ def decrypt(data, key):
         print("Data is not a valid number.")
         return None
     return reverse_concatenated_number(str(Data))
-
-# Note: Make sure to replace the data_table with the generated mapping table in your implementation.
